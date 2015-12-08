1e522b17aa6343ff06d2fe0f281b7aaadef06908
msloekito <msloekito@gmail.com>
2015-12-08 19:15:45 +0000

    Added color and age column to cats table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151208185046) do
+ActiveRecord::Schema.define(version: 20151208191503) do
 
   create_table "cats", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "color"
+    t.integer  "age"
   end
 
 end
